3bee34a551c838c06cf52b5a9ff6daa000b6f5cb
YoguGithub <yogugithub@gmail.com>
2016-09-30 12:30:04 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <?php
-$target_dir = "New folder/";
+$target_dir = "sitetrial/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
@@ -25,10 +25,10 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
+//if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+//&& $imageFileType != "gif" ) {
+//  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//$uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
